8ad4c9aca2bb13c0e8aaec82637c8394d7b5fa6e
HarbingerZou <90372397+HarbingerZou@users.noreply.github.com>
2025-10-27 21:28:46 -0700

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+    const conn = await mongoose.connect('mongodb+srv://guest:qazwsxedc@default.ktjsvq6.mongodb.net/bobyard', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
